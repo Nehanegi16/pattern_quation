b3954cce60e7488dc1560eb79a631f858f8d20e2
Neha <139878899+Nehanegi16@users.noreply.github.com>
2023-09-14 16:16:20 +0530

    Add files via upload

---
+++ star4.js
@@ -0,0 +1,15 @@
+let n=5
+let str=""
+for(let i=1;i<=n;i++)
+{
+    for(let j=1;j<=n;j++){
+        if(i==1){
+            str+="* "
+        }
+        else if(){
+            str+="  "
+        }
+    }
+    str+="\n"
+}
+console.log(str)
